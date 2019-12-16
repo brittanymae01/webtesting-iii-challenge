df405b177e2c6cf963fece1b680c4ad715087c0b
brittanymae01 <brittanymae01>
2019-12-16 13:43:55 -0800

    display testing

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,26 @@
 // Test away!
+
+import React from 'react';
+import { render } from '@testing-library/react'
+
+import Display from '../display/Display';
+
+test('correct message is rendered if locked state', () => {
+    const { getByTestId, rerender } = render(<Display locked />);
+    getByTestId('locked');
+
+    rerender(<Display locked={true} />);
+    getByTestId('locked');
+});
+
+test('correct message is rendered if closed state', () => {
+    const { getByTestId, rerender } = render(<Display closed />);
+    getByTestId('closed');
+
+    rerender(<Display closed={true} />);
+    getByTestId('closed');
+});
+
+
+
+
